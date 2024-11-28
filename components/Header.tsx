05f6134258bc833bfb2442da00be407e2026b8ce
demian.iva <demian.iva@fernir.co>
2024-11-28 11:16:21 +0200

    add changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const Header = () => {
   return (
       <div className='flex justify-between items-center p-6 text-white text-xl bg-blue-700 h-[80px]'>
-          <h2> Discover info with us ! Gain new knowledge! </h2>
-          <h2> INCORA </h2>
+          <h2> Discover info with us !</h2>
+          
           <Link className="underline" href="/">All User</Link>
     </div>
 
